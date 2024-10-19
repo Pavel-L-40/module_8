69254a457fcceba76c25aa068dade4ff43f1d5ac
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2024-10-19 09:33:23 +0300

    don't repit your self

--- m8_1_finaly.py
+++ m8_1_finaly.py
@@ -5,16 +5,18 @@ def add_everything_up(a, b):
         a + b
     except TypeError as exc:
         print(exc.args)
-        a = str(a)
-        b = str(b)
+        a, b = type_converter(a,b)
     except SyntaxError as exc:
         print(exc.args)
-        a = str(a)
-        b = str(b)
+        a, b = type_converter(a,b)
     else:
         print("it's ok")
     finally:
         return (a + b)
+
+def type_converter(a, b):
+    a,b = str(a),str(b)
+    return a, b
 
 print()
 print(add_everything_up(123.456, ' строка'), '\n')
